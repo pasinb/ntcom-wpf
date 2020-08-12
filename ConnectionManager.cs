5888bce9b3134584f8eca1e2211d9daa8d355ef6
Pasin Boonsermsuwong <panbemail@gmail.com>
2020-08-13 06:49:51 +0700

    Init commit.

---
+++ ConnectionManager.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NTCOM_WPF.Properties;
+using System.Net.Sockets;
+using System.CodeDom;
+using System.Net;
+
+namespace NTCOM_WPF
+{
+
+    public delegate void RecvMsgEventHandler(RecvMsgEvent e);
+
+    public class RecvMsgEvent
+    {
+        public string msg;
+        public RecvMsgEvent( string cmsg)
+        {
+            msg = cmsg;
+        }
+    }
+
+    public delegate void ConnectionChangedHandler(ConnectionChangedEvent e);
+
+    public class ConnectionChangedEvent
+    {
+        public string desc;
+        public ConnectionChangedEvent(string newDesc)
+        {
+            desc = newDesc;
+        }
+    }
+
+
+    class ConnectionManager : IDisposable
+    {
+        private bool _isRunning = true;
+
+        private SerialPort serial_port;
+        private UDPsocket udp_socket;
+
+        static bool autoconnecting = false;
+
+        public event RecvMsgEventHandler OnRecvMsg;
+        public event ConnectionChangedHandler OnConnectionChanged;
+
+        public ConnectionManager()
+        {
+            serial_port = new SerialPort();
+            serial_port.Parity = Parity.None;
+            serial_port.DataBits = 8;
+            serial_port.StopBits = StopBits.One;
+            serial_port.NewLine = "\r";
+            serial_port.ReadTimeout = 2000;
+            serial_port.WriteTimeout = 1500;
+            serial_port.DataReceived += new SerialDataReceivedEventHandler(OnSerialPortRecv);
+
+            udp_socket = new UDPsocket();
+
+            new Thread(TickThread).Start();
+        }
+
+        private static void OnSerialPortRecv(
+                       object sender,
+                       SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string indata = sp.ReadExisting();
+            Console.WriteLine("Serial port recv:");
+            Console.Write(indata);
+        }
+
+        public void send(string msg)
+        {
+            if (Convert.ToBoolean(Settings.Default["isUdp"]))
+            {
+                udp_socket.Send(msg);
+            }
+            else
+            {
+                serial_port.Write(msg);
+                serial_port.Write(new byte[] { 13 }, 0, 1);
+            }
+        }
+
+        static bool IsSocketConnected(Socket s)
+        {
+            try {
+                return !((s.Poll(1000, SelectMode.SelectRead) && (s.Available == 0)) || !s.Connected);
+            }
+            catch (System.ObjectDisposedException e) {
+                return false;
+            }
+        }
+
+        public void TickThread() {
+            while (_isRunning) {
+                tick();
+                Thread.Sleep(500);
+            }
+        }
+
+        public void tick()
+        {
+            if (Convert.ToBoolean(Settings.Default["isUdp"]))
+            {
+                if (serial_port.IsOpen) {
+                    serial_port.Close();
+                }
+                if (!IsSocketConnected(udp_socket.socket))
+                {
+                    // TODO try catch
+                    udp_socket.Client("127.0.0.1", 1470);
+                }
+            }
+            else  
+            {
+                // if (IsSocketConnected(udp_socket.socket)) {
+
+                    //udp_socket.Disconnect();
+                //}
+                if (!serial_port.IsOpen && Convert.ToBoolean(Settings.Default["isConnecting"]))
+                {
+                    OnConnectionChanged( new ConnectionChangedEvent(Convert.ToBoolean(Settings.Default["isConnecting"]) ? "COM port closed - trying to reconnect" : "COM port closed"));
+                    new Thread(serialAutoConnect).Start();
+                }
+                else if (serial_port.IsOpen && !Convert.ToBoolean(Settings.Default["isConnecting"])) {
+                    OnConnectionChanged( new ConnectionChangedEvent("COM port closed"));
+                    serial_port.Close();
+                }
+            }
+        }
+        private void serialAutoConnect()
+        {
+            // static variable autoconnecting prevents multiple instance of this method running to prevent conflict
+            if (serial_port.IsOpen || autoconnecting) { return; }
+            autoconnecting = true;
+
+            try
+            {
+                serial_port.BaudRate = Convert.ToInt32(Settings.Default["baudRate"]);
+            }
+            catch (Exception)
+            {
+                serial_port.BaudRate = Convert.ToInt32(Settings.Default.Properties["baudRate"].DefaultValue);
+            }
+            if (SerialPort.GetPortNames().Contains(Settings.Default["savedPort"]))
+            {
+                try
+                {
+                    serial_port.PortName = Convert.ToString(Settings.Default["savedPort"]);
+                    serial_port.DiscardInBuffer();
+                    serial_port.DiscardOutBuffer();
+                    serial_port.Open();
+                    autoconnecting = false;
+                    OnConnectionChanged( new ConnectionChangedEvent("COM port opened"));
+                    return;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.GetType() + " " + e.Message);
+                    if (serial_port.IsOpen) serial_port.Close();
+                }
+            }
+            autoconnecting = false;
+        }
+
+        public void Dispose()
+        {
+            _isRunning = false;
+        }
+
+
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace NTCOM_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+
+
+
+    public partial class MainWindow : Window
+    {
+
+        private ConnectionManager connectionManager;
+
+        public ObservableCollection<DataRow> stateGrid;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            connectionManager = new ConnectionManager();
+            connectionManager.OnRecvMsg += new RecvMsgEventHandler(HandleOnRecv);
+            connectionManager.OnConnectionChanged += new ConnectionChangedHandler(HandleConnectionChanged);
+
+            stateGrid = new ObservableCollection<DataRow>();
+            for (int i = 0; i < 8; i++)
+            {
+                stateGrid.Add(new DataRow() { 
+                    index= i+1, 
+                    data = new ObservableCollection<string> { "-", "-", "-", "-", "-", "-", "-", "-", "-", "-" }, 
+                    isRed = new ObservableCollection<bool> { false,false,false,false,false,false,false,false,false,false } 
+                });
+            }
+            mainGrid.ItemsSource = stateGrid;
+        }
+
+        private void HandleOnRecv(RecvMsgEvent e) {
+            Console.WriteLine("received handleOnRecv");
+            Console.WriteLine(e.msg);
+        }
+
+        private void HandleConnectionChanged(ConnectionChangedEvent e)
+        {
+            Console.WriteLine("received HandleConnectionChanged");
+            Console.WriteLine(e.desc);
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            connectionManager.Dispose();
+            Properties.Settings.Default.Save();
+
+            base.OnClosing(e);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new Ookii.Dialogs.Wpf.VistaFolderBrowserDialog();
+            if (dialog.ShowDialog(this).GetValueOrDefault())
+            {
+                Properties.Settings.Default.csvLocation = dialog.SelectedPath;
+            }
+        }
+
+        private void OnTabSelected(object sender, RoutedEventArgs e)
+        {
+            var tab = sender as TabItem;
+            if (tab != null)
+            {
+               // mainGrid.Items.Refresh();
+                //mainGrid.UpdateLayout();
+               // mainGrid.upd
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            stateGrid[2].data[2] = "shit";
+        }
+    }
+
+    public class RadioConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString() == parameter.ToString())
+                return true;
+            else
+                return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return System.Convert.ToBoolean(parameter);
+        }
+    }
+
+    public class NameToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (System.Convert.ToBoolean(value)) {
+                return (SolidColorBrush)(new BrushConverter().ConvertFrom("#ff9999"));
+            }
+            return DependencyProperty.UnsetValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class DataRow
+    {
+        public int index { get; set; }
+        public ObservableCollection<string> data { get; set; }
+        public ObservableCollection<bool> isRed { get; set; }
+
+    }
+}
